0debff08a9daca1d53b938dc102d6d2ec68c5d7e
Juan Daniel Ortiz <jortiz@H12129.ad.hugeinc.com>
2021-08-25 13:03:32 -0500

    Se agrega funcionalidad sticky header

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,23 @@
 import '../scss/main.scss';
 
 console.log("Hello, Webpack!");
+
+//función para capturar el evento cuando el usuario hace scroll y asignarle la clase sticky
+function sticky() {
+    var header = document.getElementsByClassName('header')[0],
+        top = 0;
+
+    function scroll(event) {
+        var s = document['documentElement' || 'body'].scrollTop;
+        if (s > top) header.classList.add('header__sticky');
+        else header.classList.remove('header__sticky');
+
+    }
+    window.addEventListener('scroll', scroll);
+}
+
+if (document.stickyState == 'complete' || document.stickyState == 'loaded') {
+    sticky();
+} else {
+    window.addEventListener('DOMContentLoaded', sticky);
+}
