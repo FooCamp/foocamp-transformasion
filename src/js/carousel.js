799127798ba0a136a4d69de074d4de86d2061d5b
Marian Quevedo <mariansofia1999@hotmail.com>
2021-10-04 19:17:10 -0500

    Created desktop version

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -3,11 +3,21 @@ window.addEventListener('DOMContentLoaded', () => {
     const cards = document.querySelectorAll('.card');
 
     cards[0].classList.add('card--active');
+    const button = document.querySelector('.carousel__button');
+    button.innerText = cards[0].dataset.buttonLabel;
+    button.setAttribute('href', cards[0].dataset.buttonUrl);
 
     cards.forEach((card, index) => {
         card.addEventListener('click', () => {
             if(card.classList.contains('card--active'))return;
-            const inactiveCardSize = card.clientWidth + 16;
+
+            const marginCard = parseFloat(getComputedStyle(card).marginLeft);
+            const inactiveCardSize = card.clientWidth + (marginCard * 2);
+            
+            let buttonUrl = card.dataset.buttonUrl;
+            let buttonLabel = card.dataset.buttonLabel;
+            button.innerText = buttonLabel;
+            button.setAttribute('href', buttonUrl);
 
             cards.forEach(otherCard => otherCard.classList.remove('card--active'));
             
